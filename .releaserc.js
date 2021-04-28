0686998c89127a3c29e5fee0de3d0573c311a6e0
Igor Cherkasov <igor.c@perfops.net>
2021-04-28 10:40:11 +0200

    fix(release): test release

---
+++ .releaserc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  branches: [ 'master' ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/github',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    [ '@semantic-release/git', {
+      'assets': [ 'package.json' ],
+      'message': 'chore(release): [skip ci] bump version to ${nextRelease.version}'
+    } ]
+  ]
+};
